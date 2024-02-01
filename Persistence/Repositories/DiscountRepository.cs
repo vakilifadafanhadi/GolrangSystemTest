36eead80013f1ad88aa5611dfdb6e2704d20a040
Hadi <vakilifadafanhadi@gmail.com>
2024-02-01 12:37:48 +0330

    Update DiscountRepository.cs

--- Persistence/Repositories/DiscountRepository.cs
+++ Persistence/Repositories/DiscountRepository.cs
@@ -3,9 +3,13 @@ using Domain.Entities;
 
 namespace Persistence.Repositories
 {
-    public sealed class CustomerRepository(DataContext dataContext) : 
-        BaseRepository<Customer>(dataContext), ICustomerRepository
+    public sealed class DiscountRepository(DataContext dataContext) : 
+        BaseRepository<Discount>(dataContext), IDiscountRepository
     {
-        ~CustomerRepository() => Dispose();
+        public async Task<ulong> SumPreFactorDiscounts(Guid preFactorHeaderId, CancellationToken cancellationToken)
+        {
+            _dataContext.Set<Discount>();
+        }
+        ~DiscountRepository() => Dispose();
     }
 }
